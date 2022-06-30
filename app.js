c801a6d57f0885c466a5e84dd5095e8257414adb
Stasiperez85 <Stasiperez85@gmail.com>
2022-06-29 22:12:20 -0600

    start game in app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    //Start the Game
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
+
+    // Call the inner functions
+    startGame();
+    updateScore();
+};
+
+game();
+
+
+
